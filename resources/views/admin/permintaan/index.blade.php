52a8709324c749886e07f0e1992759b2306d665b
FATHURRAMADHAN\fathur <fathurramadhan373@gmail.com>
2026-05-28 10:55:28 +0700

    chore:button

--- resources/views/admin/permintaan/index.blade.php
+++ resources/views/admin/permintaan/index.blade.php
@@ -391,7 +391,7 @@
                                 ${!(item.detail_paket && item.detail_paket.length > 0) ? `
                                 <button class="btn btn-xs btn-danger text-white btn-reject-action" data-id="${item.id}" data-paket="">Tolak</button>
                                 <button class="btn btn-xs btn-dark text-white btn-batal-action" data-id="${item.id}" data-paket="">Batal</button>
-                                ${currentUserRole !== 3 ? `
+                                ${currentUserRole !== '3' ? `
                                     <button class="btn btn-xs bg-orange text-white btn-confirm-action" data-id="${item.id}" data-paket="">Konfirmasi</button>
                                     <button class="btn btn-xs btn-success text-white btn-approve-action" data-id="${item.id}" data-paket="">Terima</button>
                                     ` : ''}
