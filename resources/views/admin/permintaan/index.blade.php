5de8b7252f57e6523839cb8d3d1cb04cafd58e7b
FATHURRAMADHAN\fathur <fathurramadhan373@gmail.com>
2026-06-04 16:18:40 +0700

    Refacture3

--- resources/views/admin/permintaan/index.blade.php
+++ resources/views/admin/permintaan/index.blade.php
@@ -6,7 +6,7 @@
         <style>
             .sticky-toolbar {
             position: fixed;
-            top: 100px;
+            top: 150px;
             left: 250px;
             right: 10px;
             z-index: 999;
